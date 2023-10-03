76f67cb3fd83aa42b7636f2bf3adbcdcf311ef73
Santhosh Vara Prasad Uppuluri <santhoshvaraprasad.u@gmail.com>
2023-10-03 08:33:04 -0400

    Added Database

--- main.py
+++ main.py
@@ -3,11 +3,31 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
 from tkinter import font
+import sqlite3
 import os
 import sys
 
+
+users_db_path=os.path.join(os.getcwd(), "users.db")
+
+
+def create_users_db():
+
+    if os.path.exists(users_db_path):
+        return
+    conn=sqlite3.connect(users_db_path)
+    c=conn.cursor()
+    c.execute("""CREATE TABLE users (
+        username text,
+        password text,
+        gmail text
+    )""")
+    conn.commit()
+    conn.close()
+
 class ShowTime:
     def __init__(self):
+        super().__init__()
         self.root=tk.Tk()
         self.root.title("Show Time")
         self.root.geometry("800x600")
@@ -49,7 +69,7 @@ class ShowTime:
         self.loginpage_password_entry=tk.Entry(self.loginpage, font=("Arial", 15), show="*")
         self.loginpage_password_entry.pack(pady=10)
 
-        self.loginpage_button=tk.Button(self.loginpage, text="Login", font=("Arial", 15), command=self.login)
+        self.loginpage_button=tk.Button(self.loginpage, text="Login", font=("Arial", 15), command=self.loginUser)
         self.loginpage_button.pack(pady=10)
 
         self.loginpage_back_button=tk.Button(self.loginpage, text="Back", font=("Arial", 15), command=self.show_homepage)
@@ -87,18 +107,58 @@ class ShowTime:
         self.createaccountpage_gmail_entry=tk.Entry(self.createaccountpage, font=("Arial", 15))
         self.createaccountpage_gmail_entry.pack(pady=10)
 
-        self.createaccountpage_button=tk.Button(self.createaccountpage, text="Create Account", font=("Arial", 15), command=self.create_account)
+        self.createaccountpage_button=tk.Button(self.createaccountpage, text="Create Account", font=("Arial", 15), command=self.registerUser)
         self.createaccountpage_button.pack(pady=10)
 
         self.createaccountpage_back_button=tk.Button(self.createaccountpage, text="Back", font=("Arial", 15), command=self.login_page)
         self.createaccountpage_back_button.pack(pady=10)
 
 
+    def show_mainpage(self):
+        self.clear_content()
+        self.mainpage=tk.Frame(self.root)
+        self.mainpage.pack(fill="both", expand=True)
+        self.mainpage.grid_rowconfigure(0, weight=1)
+        self.mainpage.grid_columnconfigure(0, weight=1)
+
+        self.mainpage_label=tk.Label(self.mainpage, text="Main Page", font=("Arial", 20))
+        self.mainpage_label.pack(pady=20)
+
+        self.mainpage_button=tk.Button(self.mainpage, text="Start", font=("Arial", 15), command=self.login_page)
+        self.mainpage_button.pack(pady=20)
 
 
+    def loginUser(self):
+        username=self.loginpage_username_entry.get()
+        password=self.loginpage_password_entry.get()
+        conn=sqlite3.connect(users_db_path)
+        c=conn.cursor()
+        c.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+        if c.fetchone():
+            messagebox.showinfo("Success", "Login successful")
+            self.show_mainpage()
+        else:
+            messagebox.showerror("Error", "Wrong username or password")
+            self.login_page()
+        conn.commit()
+        conn.close()
 
-    def login(self):
-        pass
+
+    def registerUser(self):
+        username=self.createaccountpage_username_entry.get()
+        password=self.createaccountpage_password_entry.get()
+        gmail=self.createaccountpage_gmail_entry.get()
+        conn=sqlite3.connect(users_db_path)
+        c=conn.cursor()
+        c.execute("SELECT * FROM users WHERE username=?", (username,))
+        if c.fetchone():
+            messagebox.showerror("Error", "Username already exists")
+        else:
+            c.execute("INSERT INTO users VALUES (?, ?, ?)", (username, password, gmail))
+            messagebox.showinfo("Success", "Account created successfully")
+            self.login_page()
+        conn.commit()
+        conn.close()
 
 
 
@@ -122,5 +182,6 @@ class ShowTime:
 
 
 if __name__=="__main__":
+    create_users_db()
     ShowTime()
     tk.mainloop()

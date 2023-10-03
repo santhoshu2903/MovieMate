cd3246316643a5a204c75a9dac214fd81189dffd
Santhosh Vara Prasad Uppuluri <santhoshvaraprasad.u@gmail.com>
2023-10-03 08:14:30 -0400

    created Main file, ShowTime

---
+++ main.py
@@ -0,0 +1,129 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import font
+import os
+import sys
+
+class ShowTime:
+    def __init__(self):
+        self.root=tk.Tk()
+        self.root.title("Show Time")
+        self.root.geometry("800x600")
+        self.show_homepage()
+
+    def show_homepage(self):
+        self.clear_content()
+        self.homepage=tk.Frame(self.root)
+        self.homepage.pack(fill="both", expand=True)
+        self.homepage.grid_rowconfigure(0, weight=1)
+        self.homepage.grid_columnconfigure(0, weight=1)
+
+        self.homepage_label=tk.Label(self.homepage, text="Show Time", font=("Arial", 20))
+        self.homepage_label.pack(pady=20)
+
+        self.homepage_button=tk.Button(self.homepage, text="Start", font=("Arial", 15), command=self.login_page)
+        self.homepage_button.pack(pady=20)
+
+
+    def login_page(self):
+        self.clear_content()
+        self.loginpage=tk.Frame(self.root)
+        self.loginpage.pack(fill="both", expand=True)
+        self.loginpage.grid_rowconfigure(0, weight=1)
+        self.loginpage.grid_columnconfigure(0, weight=1)
+
+        self.loginpage_label=tk.Label(self.loginpage, text="Login", font=("Arial", 20))
+        self.loginpage_label.pack(pady=20)
+
+        self.loginpage_username_label=tk.Label(self.loginpage, text="Username", font=("Arial", 15))
+        self.loginpage_username_label.pack(pady=10)
+
+        self.loginpage_username_entry=tk.Entry(self.loginpage, font=("Arial", 15))
+        self.loginpage_username_entry.pack(pady=10)
+
+        self.loginpage_password_label=tk.Label(self.loginpage, text="Password", font=("Arial", 15))
+        self.loginpage_password_label.pack(pady=10)
+
+        self.loginpage_password_entry=tk.Entry(self.loginpage, font=("Arial", 15), show="*")
+        self.loginpage_password_entry.pack(pady=10)
+
+        self.loginpage_button=tk.Button(self.loginpage, text="Login", font=("Arial", 15), command=self.login)
+        self.loginpage_button.pack(pady=10)
+
+        self.loginpage_back_button=tk.Button(self.loginpage, text="Back", font=("Arial", 15), command=self.show_homepage)
+        self.loginpage_back_button.pack(pady=10)
+
+        self.create_account_button=tk.Button(self.loginpage, text="Create Account", font=("Arial", 15), command=self.create_account)
+        self.create_account_button.pack(pady=10)
+
+    def create_account(self):
+        self.clear_content()
+        self.createaccountpage=tk.Frame(self.root)
+        self.createaccountpage.pack(fill="both", expand=True)
+        self.createaccountpage.grid_rowconfigure(0, weight=1)
+        self.createaccountpage.grid_columnconfigure(0, weight=1)
+
+        self.createaccountpage_label=tk.Label(self.createaccountpage, text="Create Account", font=("Arial", 20))
+        self.createaccountpage_label.pack(pady=20)
+
+        self.createaccountpage_username_label=tk.Label(self.createaccountpage, text="Username", font=("Arial", 15))
+        self.createaccountpage_username_label.pack(pady=10)
+
+        self.createaccountpage_username_entry=tk.Entry(self.createaccountpage, font=("Arial", 15))
+        self.createaccountpage_username_entry.pack(pady=10)
+
+        self.createaccountpage_password_label=tk.Label(self.createaccountpage, text="Password", font=("Arial", 15))
+        self.createaccountpage_password_label.pack(pady=10)
+
+        self.createaccountpage_password_entry=tk.Entry(self.createaccountpage, font=("Arial", 15), show="*")
+        self.createaccountpage_password_entry.pack(pady=10)
+
+
+        self.createaccountpage_gmail_label=tk.Label(self.createaccountpage, text="Gmail", font=("Arial", 15))
+        self.createaccountpage_gmail_label.pack(pady=10)
+
+        self.createaccountpage_gmail_entry=tk.Entry(self.createaccountpage, font=("Arial", 15))
+        self.createaccountpage_gmail_entry.pack(pady=10)
+
+        self.createaccountpage_button=tk.Button(self.createaccountpage, text="Create Account", font=("Arial", 15), command=self.create_account)
+        self.createaccountpage_button.pack(pady=10)
+
+        self.createaccountpage_back_button=tk.Button(self.createaccountpage, text="Back", font=("Arial", 15), command=self.login_page)
+        self.createaccountpage_back_button.pack(pady=10)
+
+
+
+
+
+    def login(self):
+        pass
+
+
+
+
+
+
+    def clear_content(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    ShowTime()
+    tk.mainloop()
